7b90b1895804e6732ad1cd9c645ec7132555281a
Billy Green <billygreen@Billys-MacBook-Pro-3.local>
2023-03-21 16:44:08 -0400

    demo work from class 3-21-23

---
+++ customerservice/src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,30 @@
+package edu.iu.p565.customerservice.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import edu.iu.p565.customerservice.model.Customer;
+
+@Repository
+public class CustomerRepository {
+
+    private List<Customer> customers;
+
+    public CustomerRepository() {
+        this.customers = new ArrayList<>();
+    }
+
+    public List<Customer> findAll(){
+        return customers;
+    }
+
+    public int create (Customer customer){
+        int id = customers.size() + 1;
+        customer.setId(id);
+        customers.add(customer);
+        return id;
+    }
+    
+}
